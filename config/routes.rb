5c1ee69db8f83e2c8142fddf0e5e9c9b80bd8237
Randy Goss <randycgoss@gmail.com>
2018-10-16 03:56:33 +0000

    Added sessions, routes, and finished up authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'articles#index'
+  resources :sessions
+  resources :users  
   resources :articles
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
